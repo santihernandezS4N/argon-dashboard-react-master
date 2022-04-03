11626517d2ebe764c7fefd5cdcae18b8e7c548f6
santiago <santiagohernandez@seven4n.com>
2022-04-03 11:01:23 -0500

    Initial commit :rocket:

---
+++ src/layouts/Admin.js
@@ -0,0 +1,75 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import { useLocation, Route, Switch, Redirect } from "react-router-dom";
+// reactstrap components
+// core components
+import Sidebar from "components/Sidebar/Sidebar.js";
+
+import routes from "routes.js";
+
+const Admin = (props) => {
+  const mainContent = React.useRef(null);
+  const location = useLocation();
+
+  React.useEffect(() => {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    mainContent.current.scrollTop = 0;
+  }, [location]);
+
+  const getRoutes = (routes) => {
+    return routes.map((prop, key) => {
+      if (prop.layout === "/admin") {
+        return (
+          <Route
+            path={prop.layout + prop.path}
+            component={prop.component}
+            key={key}
+          />
+        );
+      } else {
+        return null;
+      }
+    });
+  };
+
+
+
+  return (
+    <>
+      <Sidebar
+        {...props}
+        routes={routes}
+        logo={{
+          innerLink: "/admin/index",
+          imgSrc: require("../assets/img/brand/argon-react.png").default,
+          imgAlt: "...",
+        }}
+      />
+      <div className="main-content" ref={mainContent}>
+        <Switch>
+          {getRoutes(routes)}
+          <Redirect from="*" to="/admin/stands" />
+        </Switch>
+      </div>
+    </>
+  );
+};
+
+export default Admin;
